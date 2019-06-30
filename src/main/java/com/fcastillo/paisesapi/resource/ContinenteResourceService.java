486cb0499c07a59caa2e585e1da74deb753e6e7d
francisco castillo <cefrancastillo@gmail.com>
2019-06-30 20:09:54 -0300

    endpoints continente

--- src/main/java/com/fcastillo/paisesapi/resource/ContinenteResourceService.java
+++ src/main/java/com/fcastillo/paisesapi/resource/ContinenteResourceService.java
@@ -1,110 +1,124 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.fcastillo.paisesapi.resource;
 
+//<editor-fold defaultstate="collapsed" desc="imports">
 import com.fcastillo.paisesapi.Continente;
+import com.fcastillo.paisesapi.apimodel.ContinenteFormModel;
 import com.fcastillo.paisesapi.ejb.ContinenteFacadeLocal;
+import com.fcastillo.paisesapi.exception.ConflictException;
 import com.fcastillo.paisesapi.exception.ErrorMessage;
 import com.fcastillo.paisesapi.exception.NotFoundException;
-import com.fcastillo.paisesapi.interfaces.Operaciones;
+import com.fcastillo.paisesapi.interfaces.OperacionesContinente;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
-import java.util.List;
 import javax.ejb.EJB;
 import javax.json.Json;
-import javax.json.JsonArrayBuilder;
 import javax.json.JsonObjectBuilder;
-import javax.ws.rs.DefaultValue;
-import javax.ws.rs.FormParam;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
-import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+//</editor-fold>
 
 /**
  *
- * @author fcastillo
+ * @author Francisco Castillo
+ * @version 1.0
+ * @since Julio 2019
  */
 @Path("/Continente")
 @Api(value = "Continente")
-public class ContinenteResourceService implements Operaciones {
+public class ContinenteResourceService implements OperacionesContinente {
 
+    //<editor-fold defaultstate="collapsed" desc="fields">
     @EJB
     ContinenteFacadeLocal continenteEJB;
-    private List<Continente> lstContinente;
-    private Continente continente;
+    //</editor-fold>
 
+    //<editor-fold defaultstate="collapsed" desc="crear()">
     @Override
     @POST
-    @Path("/Crear")
-    public Response crear(
-            @ApiParam("Nombre") @FormParam("nombre") String nombreLargo,
-            @ApiParam("corto") @FormParam("corto") String nombreCorto) {
-        return Response.ok().build();
-    }
+    @Path("Crear")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @ApiOperation(value = "Añadir un nuevo continente")
+    public Response crear(@ApiParam(value = "Continente", name = "form") ContinenteFormModel form) {
 
-    @Override
-    public Response actualizar() {
-        return Response.ok().build();
-    }
+        if (continenteEJB.exists(form.getIdContinente())) {
+            ErrorMessage errorMessage = new ErrorMessage("409", "El continente con el ID: " + form.getIdContinente() + " ya se encuentra registrado", "", Response.Status.CONFLICT);
+            throw new ConflictException(errorMessage);
+        }
+        Continente continente = new Continente(form.getNombre());
+        continenteEJB.create(continente);
+        return Response.status(Response.Status.CREATED).build();
+    }//</editor-fold>
 
+    //<editor-fold defaultstate="collapsed" desc="cambiar()">
     @Override
-    public Response eliminar(int codigo) {
-        return Response.ok().build();
-    }
+    @POST
+    @Path("Cambiar")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @ApiOperation(value = "Actualizar información de un continente")
+    public Response cambiar(@ApiParam(value = "Continente", name = "form") ContinenteFormModel form) {
+        Continente continente = continenteEJB.find(form.getIdContinente());
 
-    @Override
-    @GET
-    @Path("/Get/{id}")
-    public Response obtener(@ApiParam(value = "id") @PathParam("id") int codigo) {
-        continente = continenteEJB.find(codigo);
         if (continente == null) {
-            ErrorMessage errorMessage = new ErrorMessage("404", "Continente no encontrado", "http://localhost:8080/errores/404.xhtml", Response.Status.NOT_FOUND);
+            ErrorMessage errorMessage = new ErrorMessage("404", "Continente no encontrado", "", Response.Status.NOT_FOUND);
             throw new NotFoundException(errorMessage);
         }
-        JsonObjectBuilder job = Json.createObjectBuilder().add("continente", Json.createObjectBuilder()
-                .add("id", continente.getContinenteId())
-                .add("nombre", continente.getContinenteNombre()));
-        return Response.status(Response.Status.OK).entity(job.build()).build();
-    }
 
+        continente.setContinenteId(form.getIdContinente());
+        continente.setContinenteNombre(form.getNombre());
+
+        continenteEJB.edit(continente);
+        return Response.status(Response.Status.OK).build();
+    }//</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="eliminar()">
+    @Override
+    @DELETE
+    @Path("Eliminar/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @ApiOperation(value = "Eliminar continente")
+    public Response eliminar(@ApiParam(value = "id continente", name = "id") @PathParam("id") int idContinente) {
+        Continente continente = continenteEJB.find(idContinente);
+
+        if (continente == null) {
+            ErrorMessage errorMessage = new ErrorMessage("404", "Continente no encontrado", "", Response.Status.NOT_FOUND);
+            throw new NotFoundException(errorMessage);
+        }
+
+        continenteEJB.remove(continente);
+        return Response.ok().build();
+    }//</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="obtener()">
     @Override
     @GET
-    @Path("/GetLista")
-    @ApiOperation(value = "Retorna una lista de continentes")
-    public Response obtenerTodos(
-            @ApiParam("offset") @QueryParam("offset") @DefaultValue("0") Integer offset,
-            @ApiParam("limit") @QueryParam("limit") @DefaultValue("5") Integer limit,
-            @ApiParam("search") @QueryParam("search") String search,
-            @ApiParam("sort") @QueryParam("sort") int sort,
-            @ApiParam("order") @QueryParam("order") int order
-    ) {
-        lstContinente = continenteEJB.findByParameters(offset, limit);
-        JsonArrayBuilder arregloContinente = Json.createArrayBuilder();
-        JsonObjectBuilder job;
-        if (lstContinente == null) {
-            ErrorMessage errorMessage = new ErrorMessage("404", "Continentes no encontrados", "http://localhost:8080/errores/404.xhtml", Response.Status.NOT_FOUND);
+    @Path("Get")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @ApiOperation(value = "Buscar continente por su Id")
+    public Response obtener(@ApiParam(value = "id continente", name = "idContinente") @QueryParam("idContinente") int idContinente) {
+        Continente continente = continenteEJB.find(idContinente);
+
+        if (continente == null) {
+            ErrorMessage errorMessage = new ErrorMessage("404", "Continente no encontrado", "", Response.Status.NOT_FOUND);
             throw new NotFoundException(errorMessage);
         }
-        for (Continente item : lstContinente) {
-            arregloContinente.add(Json.createObjectBuilder()
-                    .add("id", item.getContinenteId())
-                    .add("nombre", item.getContinenteNombre()));
-        }
-        job = Json.createObjectBuilder().add("continente", arregloContinente);
-        return Response.status(Response.Status.OK).entity(job.build()).build();
-    }
+        JsonObjectBuilder job = Json.createObjectBuilder();
+        job.add("id", continente.getContinenteId());
+        job.add("nombre", continente.getContinenteNombre());
 
-    @Override
-    public Response getFormData() {
-        return Response.ok().build();
-    }
+        return Response.status(Response.Status.OK).entity(job.build()).build();
+    }//</editor-fold>
 
 }
