c122f5a4ed2f3313219659534f02f35b6f8614a1
francisco castillo <cefrancastillo@gmail.com>
2019-06-30 16:01:29 -0300

    nuevos paises y continentes

--- src/main/resources/db/migration/V1__Crear_tablas.sql
+++ src/main/resources/db/migration/V1__Crear_tablas.sql
@@ -1,9 +1,18 @@
+CREATE TABLE continente(
+    continente_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    continente_nombre VARCHAR(45) NOT NULL
+);
+
 CREATE TABLE pais(
     pais_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     pais_nombre_largo VARCHAR(80) NOT NULL,
     pais_nombre_corto VARCHAR(50) NOT NULL,
     pais_abreviatura VARCHAR(5),
-    bandera VARCHAR(20)
+    pais_capital VARCHAR(50),
+    pais_poblacion INT,
+    bandera VARCHAR(40),
+    resenia VARCHAR(400),
+    continente_id INT NOT NULL
 );
 
 CREATE TABLE provincia(
@@ -14,5 +23,5 @@ CREATE TABLE provincia(
     provincia_longitud VARCHAR(400),
     pais_id INT NOT NULL
 );
-
-ALTER TABLE provincia ADD FOREIGN KEY FK_PROVINCIA_PAIS (pais_id) REFERENCES pais (pais_id) ON UPDATE CASCADE;
+ALTER TABLE pais ADD FOREIGN KEY FK_PAIS_CONTINENTE(continente_id) REFERENCES continente(continente_id) ON UPDATE CASCADE;
+ALTER TABLE provincia ADD FOREIGN KEY FK_PROVINCIA_PAIS (pais_id) REFERENCES pais (pais_id) ON Upais_id) ON UPDATE CASCADE;
