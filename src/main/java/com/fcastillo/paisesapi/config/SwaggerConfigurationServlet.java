bb19917fc3b2fbaccce1925c98ff11a5c3480aff
francisco castillo <cefrancastillo@gmail.com>
2019-07-05 11:35:24 -0300

    refactor move class swaggerconfigurationservlet

---
+++ src/main/java/com/fcastillo/paisesapi/config/SwaggerConfigurationServlet.java
@@ -0,0 +1,39 @@
+package com.fcastillo.paisesapi.config;
+
+//<editor-fold defaultstate="collapsed" desc="imports">
+import io.swagger.jaxrs.config.BeanConfig;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+//</editor-fold>
+
+/**
+ * Servlet que permite establecer distintas propiedades para para inicializar Swagger
+ *
+ * @author fcastillo
+ * @since 30 de Junio de 2019
+ * @version 2.0
+ */
+@WebServlet(name = "SwaggerConfigurationServlet", loadOnStartup = 2)
+public class SwaggerConfigurationServlet extends HttpServlet {
+
+    //<editor-fold defaultstate="collapsed" desc="init()">
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+
+        super.init(config);
+        BeanConfig beanConfig = new BeanConfig();
+
+        beanConfig.setBasePath("paisesAPI/api");
+        beanConfig.setHost("localhost:8080");
+        beanConfig.setTitle("API RESTful con JAX-RS");
+        beanConfig.setDescription("API desarrollada con propositos educativos para el taller de Interoperabilidad con Servcios Web | FCEyT - UNSE");
+        beanConfig.setContact("cefrancastillo@gmail.com");
+        beanConfig.setVersion("1.0.0");
+        beanConfig.setResourcePackage("com.fcastillo.paisesapi.resource");
+        beanConfig.setPrettyPrint(true);
+        beanConfig.setScan(true);
+        beanConfig.setSchemes(new String[]{"http"});
+    }//</editor-fold>
+}
